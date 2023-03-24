c65363115defb22b6aaa663c8fcef9d55daafc2b
Metehan <metehan.bolat.ie@gmail.com>
2023-03-24 15:44:26 +0300

    Onboarding Screen was completed with Animation Indicator

--- app/src/main/java/com/metehanbolat/onboardingscreencompose/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/onboardingscreencompose/MainActivity.kt
@@ -3,15 +3,121 @@ package com.metehanbolat.onboardingscreencompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 import com.metehanbolat.onboardingscreencompose.ui.theme.OnboardingScreenComposeTheme
+import kotlinx.coroutines.launch
 
+@ExperimentalFoundationApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             OnboardingScreenComposeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
 
+                    val pagerStrings = resources.getStringArray(R.array.pager_string_array)
+                    val state = rememberPagerState()
+
+                    HorizontalPager(
+                        state = state,
+                        pageCount = pagerStrings.size
+                    ) {
+                        val currentPageCount = it + 1
+                        Column(
+                            modifier = Modifier.fillMaxSize(),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Text(text = "Title: $currentPageCount")
+                            Text(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(horizontal = 20.dp),
+                                text = pagerStrings[it]
+                            )
+                            Spacer(modifier = Modifier.height(20.dp))
+                            val scope = rememberCoroutineScope()
+                            Button(
+                                onClick = {
+                                    scope.launch {
+                                        state.animateScrollToPage(currentPageCount)
+                                    }
+                                }
+                            ) {
+                                Text(
+                                    text = if (currentPageCount == pagerStrings.size) "Complete" else "Next"
+                                )
+                            }
+                        }
+
+                    }
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 50.dp),
+                        verticalAlignment = Alignment.Bottom,
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Indicators(size = pagerStrings.size, index = state.currentPage)
+                    }
+                }
             }
         }
     }
+}
+
+@Composable
+fun Indicators(size: Int, index: Int) {
+    Row(
+        modifier = Modifier.padding(vertical = 10.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        repeat(size) {
+            Indicator(isSelected = it == index)
+        }
+    }
+}
+
+@Composable
+fun Indicator(isSelected: Boolean) {
+
+    val width by animateDpAsState(
+        targetValue = if (isSelected) 25.dp else 10.dp,
+        animationSpec = spring(dampingRatio = Spring.DampingRatioLowBouncy)
+    )
+    Box(
+        modifier = Modifier
+            .height(10.dp)
+            .width(width = width)
+            .clip(CircleShape)
+            .background(
+                if (isSelected) MaterialTheme.colors.primary else MaterialTheme.colors.onBackground.copy(
+                    alpha = 0.5f
+                )
+            )
+    )
 }
